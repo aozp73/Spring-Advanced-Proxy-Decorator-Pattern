99d0cdd6303b782f6ac362cb793cce59a7016f20
aozp73 <aozp73@naver.com>
2023-10-30 22:24:41 +0900

    동적 프록시 기술 - PatternMatchUtils.simpleMatch 활용, 적용 메서드 제어

--- src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
+++ src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
@@ -2,6 +2,7 @@ package hello.proxy.config.v2_dynamicproxy;
 
 import hello.proxy.app.v1.*;
 import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import hello.proxy.config.v2_dynamicproxy.handler.LogTraceFilterHandler;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import java.lang.reflect.Proxy;
 
 @Configuration
-public class DynamicProxyBaisConfig {
+public class DynamicProxyFilterConfig {
+
+    private static final String[] PATTERNS = {"request*", "order*", "save*"};
 
     @Bean
     public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
@@ -18,7 +21,7 @@ public class DynamicProxyBaisConfig {
         OrderControllerV1 proxy = (OrderControllerV1)
                 Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
                         new Class[]{OrderControllerV1.class},
-                        new LogTraceBasicHandler(orderController, logTrace)
+                        new LogTraceFilterHandler(orderController, logTrace, PATTERNS)
                 );
         return proxy;
     }
@@ -29,7 +32,7 @@ public class DynamicProxyBaisConfig {
         OrderServiceV1 proxy = (OrderServiceV1)
                 Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(),
                         new Class[]{OrderServiceV1.class},
-                        new LogTraceBasicHandler(orderService, logTrace)
+                        new LogTraceFilterHandler(orderService, logTrace, PATTERNS)
                 );
         return proxy;
     }
@@ -40,7 +43,7 @@ public class DynamicProxyBaisConfig {
         OrderRepositoryV1 proxy = (OrderRepositoryV1)
                 Proxy.newProxyInstance(OrderRepositoryV1.class.getClassLoader(),
                         new Class[]{OrderRepositoryV1.class},
-                        new LogTraceBasicHandler(orderRepository, logTrace)
+                        new LogTraceFilterHandler(orderRepository, logTrace, PATTERNS)
                 );
         return proxy;
     }
