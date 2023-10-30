3aefa15ef08492ef67ed101dc7812e52fcdfa2d4
aozp73 <aozp73@naver.com>
2023-10-30 20:06:48 +0900

    동적 프록시 기술 - 리플렉션

--- src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -1,6 +1,7 @@
 package hello.proxy.app.v2;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
